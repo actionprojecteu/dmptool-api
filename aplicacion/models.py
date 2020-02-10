88461f04274ecd3e633b6e8b73912be98fc18df6
RafaIG <rafael.ines.guillen@gmail.com>
2020-02-10 09:50:16 +0100

    Primera version con tokens

--- aplicacion/models.py
+++ aplicacion/models.py
@@ -1,8 +1,11 @@
-from sqlalchemy import Boolean, Column, ForeignKey
-from sqlalchemy import DateTime, Integer, String, Text, Float
+from sqlalchemy import Boolean, Column, ForeignKey, DateTime, Integer,\
+    String, Text, Float
 from sqlalchemy.orm import relationship, backref
 from aplicacion.app import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from itsdangerous import (TimedJSONWebSignatureSerializer
+                          as Serializer, BadSignature, SignatureExpired)
+
 
 class Projects(db.Model):
     __tablename__ = 'projects'
@@ -37,6 +40,22 @@ class Users(db.Model):
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def generate_auth_token(self, expiration=1800):
+        s = Serializer(app.config['SECRET_KEY'], expires_in=expiration)
+        return s.dumps({'id': self.id})
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            return None    # valid token, but expired
+        except BadSignature:
+            return None    # invalid token
+        user = Users.query.get(data['id'])
+        return user
 
     # Flask-Login integration
     def is_authenticated(self):
