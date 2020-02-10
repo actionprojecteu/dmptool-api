1b2feedfeef5b237cd44805f5efb7783583520ac
RafaIG <55830012+RafaIG@users.noreply.github.com>
2020-02-10 09:54:26 +0100

    Minor change in the configuration file

--- aplicacion/config.py
+++ aplicacion/config.py
@@ -5,14 +5,14 @@ SECRET_KEY = 'this is the secret key EGG&RIG'
 PWD = os.path.abspath(os.curdir)
 
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}/dbase.db'.format(PWD)
-#SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:{}@{}/dmpsqlite'.format(os.environ["MYSQL_PASSWORD"],os.environ["MYSQL_PORT_3306_TCP_ADDR"])
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///{}/dbase.db'.format(PWD)
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:{}@{}/dmpsqlite'.format(os.environ["MYSQL_PASSWORD"],os.environ["MYSQL_PORT_3306_TCP_ADDR"])
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(hours=24)
 
-#MONGO_URI = 'mongodb://mongodb:27017/dmptool'
-MONGO_URI = 'mongodb://localhost:27017/dmptool'
+MONGO_URI = 'mongodb://mongodb:27017/dmptool'
+#MONGO_URI = 'mongodb://localhost:27017/dmptool'
 
 CORS_HEADERS = 'Content-Type'
 
